6ec6e7185f2b1974275013e8af3a05b44e18ddcd
KevinLin02 <puddingodgin61@gmail.com>
2023-08-24 16:33:21 +0800

    0824 inclass commit

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,74 +1,84 @@
 /* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
-import { Table } from 'antd';
-import { users } from '../../api/users';
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'first_name',
-    key: 'first_name',
-  },
-  {
-    title: 'Last Name',
-    dataIndex: 'last_name',
-    key: 'last_name',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-  },
-  {
-    title: 'Avatar',
-    key: 'avatar',
-    dataIndex: 'avatar',
-    render: (avatar) => <img src={avatar} alt="Avatar" style={{ width: '40px' }} />,
-  },
-];
+import {
+  Table, Button, Modal, Input, Form,
+} from 'antd';
+import { useUsers } from '../../api/users';
 
 export default function UsersPage() {
-  const [data, setData] = useState([]);
-  const [tableParams, setTableParams] = useState({
-    pagination: {
-      current: 1,
-      pageSize: 6,
+  const {
+    users, createUser, updateUserById, deleteUserById,
+  } = useUsers();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'first_name',
+      key: 'first_name',
     },
-  });
-
-  const fetchData = async () => {
-    const result = await users();
-    const usersData = result?.data?.data || [];
-    setData(usersData);
-    setTableParams({
-      ...tableParams,
-      pagination: {
-        ...tableParams.pagination,
-        total: 12,
-      },
-    });
+    {
+      title: 'Last Name',
+      dataIndex: 'last_name',
+      key: 'last_name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Avatar',
+      key: 'avatar',
+      dataIndex: 'avatar',
+      render: (avatar) => <img src={avatar} alt="Avatar" style={{ width: '40px' }} />,
+    },
+    {
+      title: 'Operation',
+      key: 'operation',
+      dataIndex: 'operation',
+      render: (_, user) => (
+        <>
+          <Button onClick={() => openEditModal()} style={{ marginRight: '10px' }}> create </Button>
+          <Button onClick={() => updateUserById(user.id, ...user)} style={{ marginRight: '10px' }}> update </Button>
+          <Button onClick={() => deleteUserById(user.id)}> delete </Button>
+        </>
+      ),
+    },
+  ];
+  const openEditModal = () => {
+    setIsModalOpen(true);
   };
-
-  useEffect(() => {
-    fetchData();
-  }, [JSON.stringify(tableParams)]);
-
-  const handleTableChange = (pagination, filters, sorter) => {
-    setTableParams({
-      pagination,
-      filters,
-      ...sorter,
-    });
-
-    // `dataSource` is useless since `pageSize` changed
-    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-      setData([]);
-    }
-  };
-
   return (
-    <div>
-      <Table columns={columns} dataSource={data} pagination={tableParams.pagination} onChange={handleTableChange} />
-    </div>
+    <>
+      <Modal title="EditModal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Form>
+          <Form.Item
+            name="Name"
+            label="name"
+            rules={[
+              {
+                type: 'name',
+              },
+              {
+                required: true,
+                message: 'Please input your Name',
+              },
+            ]}
+          />
+          <Input />
+        </Form>
+      </Modal>
+      <div>
+        <Table columns={columns} dataSource={users} />
+      </div>
+    </>
   );
 }
